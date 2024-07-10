196d0726e295c4a4a5625bcda33288e2243adeb3
MariuAndrade <mrandrad@fiec.espol.edu.ec>
2024-07-10 16:36:19 -0500

    decorator
    clases y funciones

---
+++ Taller07_Patrones_C-E/src/main/java/ec/edu/espol/taller07_patrones_c/e/Builder/fontDecorator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.taller07_patrones_c.e.Builder;
+
+/**
+ *
+ * @author CltControl
+ */
+public class fontDecorator extends InformeDecorator {
+    
+    public void generarReporte(){};
+    public void visualizarReporte(){};
+    
+    public void aplicarFuente(){};
+}
