e1f5f35fd88a817fdfbd62ce69bf3eaf61e79c25
} <>
2024-07-10 16:51:40 -0500

    update

--- Taller07_Patrones_C-E/src/main/java/ec/edu/espol/taller07_patrones_c/e/Decorator/fontDecorator.java
+++ Taller07_Patrones_C-E/src/main/java/ec/edu/espol/taller07_patrones_c/e/Decorator/fontDecorator.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ec.edu.espol.taller07_patrones_c.e.Builder;
+package ec.edu.espol.taller07_patrones_c.e.Decorator;
+
+import ec.edu.espol.taller07_patrones_c.e.Decorator.InformeDecorator;
 
 /**
  *
